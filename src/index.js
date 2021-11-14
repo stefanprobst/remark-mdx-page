81bea8914ad424a7def0fd7c1d21e67faa02e6f2
Stefan Probst <stefanprobst@kolabnow.com>
2021-11-14 19:00:29 +0100

    fix: make transformer sync

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { promises as fs } from 'fs'
+import { readFileSync } from 'fs'
 
 import { Parser } from 'acorn'
 import jsx from 'acorn-jsx'
@@ -8,10 +8,10 @@ const jsxParser = Parser.extend(jsx())
 export default function attacher(options) {
   if (options.pathname == null) return undefined
 
-  return async function transformer(tree) {
-    const fileContent = await fs.readFile(options.pathname, { encoding: 'utf-8' })
-    const estree = jsxParser.parse(fileContent, { sourceType: 'module', ecmaVersion: 'latest' })
+  const fileContent = readFileSync(options.pathname, { encoding: 'utf-8' })
+  const estree = jsxParser.parse(fileContent, { sourceType: 'module', ecmaVersion: 'latest' })
 
+  return function transformer(tree) {
     tree.children.push({
       type: 'mdxjsEsm',
       data: { estree },
